7eb6dc0589209820a2e6c096bb9c184a4a948858
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-02-16 12:45:21 +0100

    Add test for word_count

---
+++ tests/test_word_count.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import sys
+
+here = Path(__file__).parent
+sys.path.append((here / ".." / "code").as_posix())
+
+from count import count_words
+
+
+def test_word_count():
+    assert count_words("hello hello world") == {"hello": 2, "world": 1}
